2c5d499edc2bf7cab842039c500e6c76d09ad8d1
lovekod2hj <s0215@paran.com>
2025-12-09 15:25:32 +0900

    Initial commit

---
+++ src/test/java/com/example/thymeleaf_template/ThymeleafTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
