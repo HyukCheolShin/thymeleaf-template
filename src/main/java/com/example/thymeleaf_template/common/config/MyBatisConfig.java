7959bd402b3a34440ae15155a5fba7f5af50a30a
lovekod2hj <s0215@paran.com>
2025-12-23 17:04:13 +0900

    MyBatis 사용을 위한 설정 추가

---
+++ src/main/java/com/example/thymeleaf_template/common/config/MyBatisConfig.java
@@ -0,0 +1,43 @@
+package com.example.thymeleaf_template.common.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.example.thymeleaf_template.**.mapper")
+public class MyBatisConfig {
+
+    private final ApplicationContext applicationContext;
+
+    public MyBatisConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setMapperLocations(applicationContext.getResources("classpath:mapper/**/*.xml"));
+        factoryBean.setTypeAliasesPackage("com.example.thymeleaf_template.**.model");
+
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setJdbcTypeForNull(org.apache.ibatis.type.JdbcType.NULL);
+        configuration.setDefaultStatementTimeout(30);
+        factoryBean.setConfiguration(configuration);
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
