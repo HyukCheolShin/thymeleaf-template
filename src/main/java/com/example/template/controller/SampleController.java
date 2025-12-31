2022fabf6ccfac097c56f09fb83a04e86416774f
johnny-gill <uint777@gmail.com>
2025-12-31 11:09:46 +0900

    feat: 목록, 등록, 수정 화면 분리

--- src/main/java/com/example/template/controller/SampleController.java
+++ src/main/java/com/example/template/controller/SampleController.java
@@ -1,15 +1,31 @@
 package com.example.template.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 화면 이동을 담당하는 컨트롤러
+ */
 @Controller
 @RequestMapping("/samples")
 public class SampleController {
 
-    @GetMapping
+    @GetMapping("")
     public String list() {
         return "sample/list";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "sample/form";
+    }
+
+    @GetMapping("/{id}")
+    public String form(@PathVariable Long id, Model model) {
+        model.addAttribute("id", id);
+        return "sample/form";
     }
 }
