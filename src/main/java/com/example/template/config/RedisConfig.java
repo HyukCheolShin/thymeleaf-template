11885513086884a4c8456f0fe2edb7c2cf91ee95
johnny-gill <uint777@gmail.com>
2026-01-05 14:55:17 +0900

    feat: Redis value를 json으로 읽을 수 있게 설정

--- src/main/java/com/example/template/config/RedisConfig.java
+++ src/main/java/com/example/template/config/RedisConfig.java
@@ -1,12 +1,30 @@
 package com.example.template.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @Profile("redis")
 @EnableRedisHttpSession
 public class RedisConfig {
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(SecurityJackson2Modules.getModules(RedisConfig.class.getClassLoader()));
+        mapper.activateDefaultTyping(
+                mapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY);
+        return new GenericJackson2JsonRedisSerializer(mapper);
+    }
 
 }
