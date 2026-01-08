a5d2c82628c4dcb20f2dbdc4093d928ff04a79ae
johnny-gill <uint777@gmail.com>
2026-01-08 18:10:50 +0900

    고고

--- src/main/java/com/example/template/controller/LoginController.java
+++ src/main/java/com/example/template/controller/LoginController.java
@@ -7,12 +7,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 
 @Controller
+@Slf4j
 public class LoginController {
 
     @GetMapping({ "/", "/login" })
     public String index(Authentication authentication) {
         if (authentication != null && authentication.isAuthenticated()
                 && !(authentication instanceof AnonymousAuthenticationToken)) {
+            logger.info("User is authenticated");
             return "redirect:/users";
         }
         return "login";
